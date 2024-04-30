887ee90c6c4c5b307a6d5f8977d4eab1dacc7bfe
albertina777 <66668664+albertina777@users.noreply.github.com>
2024-04-30 11:39:09 +0530

    Update Consumer.java

--- src/main/java/kafkaExample/Consumer.java
+++ src/main/java/kafkaExample/Consumer.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 public class Consumer {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap:9092");
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
